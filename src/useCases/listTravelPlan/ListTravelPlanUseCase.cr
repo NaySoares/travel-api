8d70a01416f7a50cff482bb86958af556ee1f5a1
Elienai Soares <elienay.soares07@gmail.com>
2023-06-21 15:14:33 -0300

    fix structure

--- src/useCases/listTravelPlan/ListTravelPlanUseCase.cr
+++ src/useCases/listTravelPlan/ListTravelPlanUseCase.cr
@@ -16,9 +16,8 @@ class ListTravelPlanController
   def self.execute() 
     db = DatabaseManager.connection
     list_travel_plans = Array(TravelStop).new
-    
-    db.query "SELECT id, travel_stops FROM travels ORDER BY id DESC" do |rs|
-      try do
+    try do
+      db.query "SELECT id, travel_stops FROM travels ORDER BY id DESC" do |rs|
         rs.each do 
           id = rs.read(Int64)
           travel_stops = rs.read(Array(Int32))
@@ -27,9 +26,9 @@ class ListTravelPlanController
       
           list_travel_plans.push(result)
         end
-      rescue e
-        puts "Error while listing travel plans: #{e}"
       end
+    rescue e
+      puts "Error while listing travel plans: #{e}"
     end
     db.close
     return list_travel_plans
