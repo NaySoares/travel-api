0200b6c95265f8c21c0dfdec79ff31f947ddf4d0
Elienai Soares <elienay.soares07@gmail.com>
2023-06-21 15:08:29 -0300

    fix listing

--- src/useCases/listTravelPlan/ListTravelPlanUseCase.cr
+++ src/useCases/listTravelPlan/ListTravelPlanUseCase.cr
@@ -1,32 +1,38 @@
 require "../../db/*"
+require "json"
 
+alias Id = Int64
+
+abstract struct StopPoint
+  include JSON::Serializable
+end
+
+struct TravelStop < StopPoint
+  getter id, travel_stops
+  def initialize(@id : Id, @travel_stops : Array(Int32)); end
+end
 
 class ListTravelPlanController 
-   struct TravelStop
-    property id : Int64
-    property travel_stops : Array(Int32)
-
-    def initialize(@id : Int64, @travel_stops : Array(Int32))
-    end
-  end
-
   def self.execute() 
     db = DatabaseManager.connection
     list_travel_plans = Array(TravelStop).new
+    
     db.query "SELECT id, travel_stops FROM travels ORDER BY id DESC" do |rs|
-      rs.each do 
-        id = rs.read(Int64)
-        travel_stops = rs.read(Array(Int32))
-        list_travel_plans << TravelStop.new(id, travel_stops)
+      try do
+        rs.each do 
+          id = rs.read(Int64)
+          travel_stops = rs.read(Array(Int32))
 
+          result = TravelStop.new(id, travel_stops)
+      
+          list_travel_plans.push(result)
+        end
+      rescue e
+        puts "Error while listing travel plans: #{e}"
       end
     end
-    return list_travel_plans.to_s
-    
-    #rescue e    
-    #  puts "Error while listing travel plans: #{e}"
-    #end
-
     db.close
+    return list_travel_plans
+  
   end
 end
