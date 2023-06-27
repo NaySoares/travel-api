59ff78f31105c419e7b4c23b838866d2e095b3c4
Elienai <elienay.soares07@gmail.com>
2023-06-26 21:54:21 -0300

    refactor

--- sam.cr
+++
@@ -1,9 +0,0 @@
-require "jennifer"
-require "jennifer/adapter/postgres"
-
-require "./config/initializers/*"
-require "./src/db/migrations/*"
-require "sam"
-require "jennifer/sam"
-load_dependencies "jennifer"
-Sam.help
